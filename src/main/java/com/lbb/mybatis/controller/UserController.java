3a5bf5b45f3771384a7ac6e6e8891fe19f4ee9b4
Lubb <414107871@qq.com>
2017-07-28 14:37:37 +0800

    去除不需要引入的包、新增注释

--- src/main/java/com/lbb/mybatis/controller/UserController.java
+++ src/main/java/com/lbb/mybatis/controller/UserController.java
@@ -4,7 +4,6 @@ import com.lbb.mybatis.model.User;
 import com.lbb.mybatis.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +13,9 @@ import java.util.List;
 
 
 /**
- * Created by bing on 2017/7/28.
+ * @author lubingbing
+ * @date 2017-7-28
+ * 用户管理控制器
  */
 @Controller
 public class UserController {
